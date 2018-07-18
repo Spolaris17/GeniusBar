f871030124334fb5bef969a581add1203f850b90
eleSerenity <770698677@qq.com>
2018-07-18 09:31:07 +0800

    creat table

---
+++ Models/RepairCenter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeniusBar.Models
+{
+    public class RepairCenter
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Address { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Tel { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string TIMG_url { get; set; }
+    }
+}
